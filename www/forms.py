bf56b756da9e2ab0fcd4d24d4118dc7b0002a566
itprohelper <rudy@itprohelper.com>
2020-05-25 18:47:02 -0400

    validation works, but does not show errors in modal form to user

--- www/forms.py
+++ www/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, IntegerField, SubmitField, DateField, TextAreaField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, NumberRange
 from wtforms.fields.html5 import DateField
 
 
@@ -16,7 +16,7 @@ class SignupForm(FlaskForm):
 
 class NewReading(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', validators=[InputRequired()])
-    systolic = IntegerField('Systolic', validators=[InputRequired()])
-    diastolic = IntegerField('Diastolic', validators=[InputRequired()])
+    systolic = IntegerField('Systolic', validators=[InputRequired(), NumberRange(min=60, max=190, message="no good")])
+    diastolic = IntegerField('Diastolic', validators=[InputRequired(), NumberRange(min=50, max=140, message="oh oh")])
     notes = TextAreaField('Notes', validators=[InputRequired(), Length(max=120)])
     submit = SubmitField('Submit')
