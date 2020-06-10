829295ba37261f109ccdf81c9d1861f44a0868a1
itprohelper <rudy@itprohelper.com>
2020-06-10 00:49:27 -0400

    models now work with sqlalchemy commands

--- www/models.py
+++ www/models.py
@@ -8,32 +8,32 @@ from sqlalchemy.orm import relationship, validates, sessionmaker
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy import create_engine
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
     join_date = db.Column(db.DateTime)
-    picture = db.Column(db.String(20))
-    reading = db.relationship('Readings', backref='user', lazy='dynamic')
+    picture = db.Column(db.String(20), nullable=False, default='default.jpg')
+    reading = db.relationship('Readings', backref='patient', lazy='dynamic')
 
     def __repr__(self):
-        return '<User %r>' % self.username
-        #return f"User('{self.username}', '{self.email}'', '{self.image_file}')"
+        # return '<User %r>' % self.username, self.email, self.picture
+        return f"User('{self.username}', '{self.email}', '{self.picture}')"
 
 class Readings(db.Model):
     __tablename__ = 'readings'
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime())
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     systolic = db.Column(db.Integer())
     diastolic = db.Column(db.Integer())
     notes = db.Column(db.String(250))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
 
     def __repr__(self):
-        return '<Readings $r>' % self.date, self.systolic, self.diastolic, self.notes
+        return f"Readings('{self.date}', '{self.systolic}', '{self.diastolic}', '{self.notes}')"
 
 class Doctor(db.Model):
     __tablename__ = 'doctor'
