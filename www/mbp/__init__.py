5ab007dfd8e1d4169d468f1a819b5ec9f7782ea3
rgm <rgm@nuevayork.local>
2022-09-06 17:25:03 -0400

    integratin sqlite, sqlalchemy and models

--- www/mbp/__init__.py
+++ www/mbp/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7930d939680d2fd3af6576162305e65f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
--- www/mbp/models.py
+++ www/mbp/models.py
@@ -1,1 +1,12 @@
+from email.policy import default
 from mbp import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
