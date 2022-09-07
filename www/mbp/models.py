f4e0f183e4e26f054e07c265f586404626c5c6b0
rgm <rgm@nuevayork.local>
2022-09-07 17:21:22 -0400

    added sqlite and tested it. need to fix error in models AttributeError: 'Readings' object has no attribute 'username'

--- www/mbp/models.py
+++ www/mbp/models.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from mbp import db
 
+
 #Database model definitions:
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
     readings = db.relationship('Readings', backref='user', lazy=True)
 
     def __repr__(self):
@@ -22,4 +23,4 @@ class Readings(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Readings('{self.username}','{self.sys}', '{self.dia}', '{self.notes}, '{self.date_posted}'')"
+        return f"Readings('{self.sys}', '{self.dia}', '{self.notes}, '{self.date_posted}'')"
