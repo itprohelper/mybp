b3baed0f5a70ca1258e2064143d7a80067f4b58c
itprohelper <rudy@itprohelper.com>
2022-09-06 23:19:14 -0400

    adding db using sqlite. Models giving some errors

--- www/mbp/__init__.py
+++ www/mbp/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7930d939680d2fd3af6576162305e65f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
 
 from mbp import views
--- www/mbp/models.py
+++ www/mbp/models.py
@@ -1,12 +1,25 @@
-from email.policy import default
+from datetime import datetime
 from mbp import db
 
+#Database model definitions:
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
     password = db.Column(db.String(60), unique=True, nullable=False)
+    readings = db.relationship('Readings', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Readings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sys = db.Column(db.String(3), nullable=False)
+    dia = db.Column(db.String(3), nullable=False)
+    notes = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Readings('{self.username}','{self.sys}', '{self.dia}', '{self.notes}, '{self.date_posted}'')"
