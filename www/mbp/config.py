71b3b2d8f53b18ac8f85692b2a9973651b760d1d
rgm <rgm@nuevayork.local>
2022-10-20 17:26:23 -0400

    config values

--- www/mbp/config.py
+++ www/mbp/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    #SECRET_KEY = '7930d939680d2fd3af6576162305e65f'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
