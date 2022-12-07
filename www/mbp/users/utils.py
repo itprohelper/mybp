4b1094cd964ca42d79ce083e6e57bd0e3720550f
rgm <rgm@nuevayork.local>
2022-12-07 17:10:57 -0500

    configure nginx as reverse proxy. need to fix errors sending reset password emails

--- www/mbp/users/utils.py
+++ www/mbp/users/utils.py
@@ -26,7 +26,7 @@ def save_picture(form_picture):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Requet',
+    msg = Message('Password Reset Request',
                     sender='mbp@itprohelper.com',
                     recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
