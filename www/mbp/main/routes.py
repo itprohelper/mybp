3a732bbd1e3b68ed9d460e8a318624139939390c
rgm <rgm@nuevayork.local>
2023-02-07 17:15:43 -0500

    setting a dashboard to display only the logged in user's readings.

--- www/mbp/main/routes.py
+++ www/mbp/main/routes.py
@@ -7,7 +7,7 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 def home():
     page = request.args.get('page', 1, type=int) #Grab the page we want. In this case page one. Set type integer as the page number.
-    reading = Reading.query.order_by(Reading.date_posted.desc()).paginate(page=page, per_page=5) #Show 5 readings per page. Can use http://localhost:8000/home?page=3 to navigate to pages.
+    reading = Reading.query.order_by(Reading.date_posted.desc()).paginate(page=page, per_page=6) #Show 5 readings per page. Can use http://localhost:8000/home?page=3 to navigate to pages.
     return render_template('index.html', reading=reading)
 
 
