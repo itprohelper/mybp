956a6a608daa8a1e5b57cf31e910493dea8c25b9
rgm <rgm@nuevayork.local>
2025-04-29 09:18:42 -0400

    registration form with no username

--- www/mbp/models.py
+++ www/mbp/models.py
@@ -11,7 +11,7 @@ def load_user(user_id):
 #Database model definitions:
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    #username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
@@ -32,7 +32,8 @@ class User(db.Model, UserMixin):
         return User.query.get(user_id)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.email}', '{self.image_file}')"
+        #return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Reading(db.Model):
     id = db.Column(db.Integer, primary_key=True)
