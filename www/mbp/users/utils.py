91876a8c40f5fa6199c35a6d291db63fad3bcae9
rgm <rgm@nuevayork.local>
2025-06-02 15:50:05 -0400

    can now send reset pass emails with yakahu.com domain

--- www/mbp/users/utils.py
+++ www/mbp/users/utils.py
@@ -28,7 +28,7 @@ def save_picture(form_picture):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                    sender='hello@itprohelper.com',
+                    sender='april@yakahu.com',
                     recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
